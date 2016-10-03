6079d7a6671c815c2bf6544f37fda6381ff72930
Vegard Kvernelv <vkvernelv@gmail.com>
2016-10-03 20:54:11 +0200

    Renamed package

--- pyfmm/__init__.py
+++ pyfmm/__init__.py
@@ -20,4 +20,4 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from fmm.solvers import march
+from pyfmm.solvers import march
--- setup.py
+++ setup.py
@@ -6,10 +6,11 @@ setup(
   packages = ['pyfmm'], # this must be the same as the name above
   version = '0.1',
   description = 'Python module implementing the Fast Marching Method.',
+  #long_description=open('README.md', 'rt').read(),
   author = 'Vegard Kvernelv',
   author_email = 'vkvernelv@gmail.com',
   url = 'https://github.com/vegardkv/pyfmm', # use the URL to the github repo
   download_url = 'https://github.com/vegardkv/pyfmm/tarball/0.1', # I'll explain this in a second
-  keywords = ['fast marching method', 'eikonal', 'fmm', 'distance'], # arbitrary keywords
+  keywords = ['fast marching method', 'eikonal', 'pyfmm', 'distance'], # arbitrary keywords
   classifiers = [],
 )
