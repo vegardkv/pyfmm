fa38fb4be1eb36a0682eb0c68eb95286a48a6c65
Vegard Kvernelv <vkvernelv@gmail.com>
2016-10-11 20:11:53 +0200

    Updated setup for new tag

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from distutils.core import setup
 setup(
   name = 'pyfmm',
   packages = ['pyfmm'], # this must be the same as the name above
-  version = '0.2.1',
+  version = '0.3',
   description = 'Python module implementing the Fast Marching Method.',
   #long_description=open('README.md', 'rt').read(),
   author = 'Vegard Kvernelv',
   author_email = 'vkvernelv@gmail.com',
   url = 'https://github.com/vegardkv/pyfmm', # use the URL to the github repo
   download_url = 'https://github.com/vegardkv/pyfmm/tarball/0.2.1', # I'll explain this in a second
-  keywords = ['fast marching method', 'eikonal', 'pyfmm', 'distance'], # arbitrary keywords
+  keywords = ['fast marching method', 'eikonal', 'fmm', 'distance'], # arbitrary keywords
   license='MIT',
-  classifiers = [],
+  classifiers=[],
 )
